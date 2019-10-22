dffb285593c9dc680125b35b72a08e1438aea0ff
imroner <46581220+imroner@users.noreply.github.com>
2019-10-22 09:56:38 +0700

    Add files via upload

---
+++ latihan1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dasarpemrograman;
+
+/**
+ *
+ * @author Moch Lutfi
+ */
+public class latihan1 {
+    public static void main(String[] args){
+        
+        //ini adalah perinth untuk menampilkan data
+        /*comentar 1
+        
+        */
+        /*
+        System.out.println("Selamad datang imron");
+                */
+        System.out.println("Prodi Teknik Informdatika");
+    }
+}
---
+++ latihan2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dasarpemrograman;
+
+/**
+ *
+ * @author Moch Lutfi
+ */
+public class latihan2 {
+public static void main(String[] args){
+//contoh data variabel string    
+String nama="imron";
+String Alamat="pandaan pasuruan";
+int umur=10;
+String jurusan="Teknik informatika";
+System.out.println(nama);
+System.out.println(umur);
+System.out.println(Alamat);
+System.out.println(jurusan);
+
+    }    
+}
